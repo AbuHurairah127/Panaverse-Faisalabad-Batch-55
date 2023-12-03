f13d834af30443c4aca7925a0e267d1357d3b878
abuhurairah127 <abuhurairahMuhammadShafique@gmail.com>
2023-12-03 17:24:30 +0500

    class 4 code added

---
+++ typescript/class4/index.js
@@ -0,0 +1,148 @@
+"use strict";
+// var name1: string = "Abu Hurairah";
+// var age1: number = 10;
+// console.log("name1 before if", name1);
+// // Conditional
+// // If - statement
+// var result1: boolean = age1 > 18;
+// // var result2: boolean = !result1;
+// if (age1 > 18) {
+//   console.log("age1", age1);
+//   console.log("age1", age1);
+//   console.log("age1", age1);
+// }
+// console.log("name1 after if", name1);
+// var marks: number = 85;
+// if (marks >= 90) {
+//   console.log("A+ Grade");
+// } else if (marks >= 50) {
+//   console.log("C Grade");
+// } else if (marks >= 80) {
+//   console.log("A grade");
+// } else if (marks >= 70) {
+//   console.log("B Grade");
+// }
+// if (marks > 50) {
+//   console.log("pass");
+// } else {
+//   console.log("fail");
+// }
+// var year: number = 2023;
+// if (year % 4 === 0) {
+//   console.log("Leap year");
+// } else {
+//   console.log("Not a leap year");
+// }
+// var nationality
+// var attendance_percentage: number = 76;
+// var theory_marks: number = 76;
+// var practical_marks: number = 76;
+// if (attendance_percentage > 75) {
+//   console.log("You are allowed to sit in exam");
+//   if (theory_marks > 50) {
+//     console.log("pass in theory");
+//     if (practical_marks > 50) {
+//       console.log("Passed Over all");
+//     } else {
+//       console.log("fail in practical");
+//     }
+//   } else {
+//     console.log("fail in theory");
+//   }
+// } else {
+//   console.log("you're not eligible for the exams");
+// }
+// var units: number = 200;
+// // 50 - 2
+// // 100 - 3
+// // 200 - 4
+// // 500 - 5
+// if (units >= 0 && units < 50) {
+//   console.log(units * 2);
+// } else if (units >= 50 && units <= 100) {
+//   var price: number = units * 3;
+//   var tax: number = price * 0.25;
+//   price += tax;
+//   console.log(price);
+// } else if (units > 100 && units <= 200) {
+//   var price = units * 4;
+//   var tax = price * 0.5;
+//   price += tax;
+//   console.log(price);
+// } else if (units > 200 && units <= 500) {
+//   var price: number = units * 5;
+//   var tax = price * 0.75;
+//   price += tax;
+//   console.log(price);
+// } else if (units > 500) {
+//   var price: number = units * 6;
+//   var tax: number = price;
+//   price += tax;
+//   console.log("price", price);
+// } else {
+//   console.log("invalid number");
+// }
+// Create a program that simulates a simple ATM. Ask the user for their account balance, and then check if they have enough funds to withdraw a certain amount. If yes, update the balance; otherwise, display an insufficient funds message.
+// var accountBalance: number = 15000;
+// var withdrawAmount: number = 10000;
+// if (accountBalance >= withdrawAmount) {
+//   console.log(
+//     `you have withdrew ${withdrawAmount} and your remaining balance is ${
+//       accountBalance - withdrawAmount
+//     }`
+//   );
+// } else {
+//   console.log(`sorry, you don't have enough money in your account`);
+// }
+// Design a program +
+// at takes a month as input (1-12) and prints the number of days in that month.
+// var month: number = 7;
+// if (
+//   month === 1 ||
+//   month === 3 ||
+//   month === 5 ||
+//   month === 7 ||
+//   month === 8 ||
+//   month === 10 ||
+//   month === 12
+// ) {
+//   console.log("days are 31");
+// } else if (month === 2) {
+//   console.log("Days are 28");
+// } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+//   console.log("days are 30");
+// } else {
+//   console.log("Invalid month");
+// }
+// Create a program that asks the user to enter three numbers. Determine and print whether the sum of any two numbers is equal to the third.
+// var num1: number = 10;
+// var num2: number = 20;
+// var num3: number = 50;
+// if (num1 + num2 === num3 || num2 + num3 === num1 || num3 + num1 === num2) {
+//   console.log("sum matched");
+// } else {
+//   console.log("sum did not match");
+// }
+// ===========
+// switch case
+var weekday = 2;
+switch (weekday) {
+    case 1: {
+        console.log("Monday");
+    }
+    case 2: {
+        console.log("tuesday");
+    }
+    case 3: {
+        console.log("wednesday");
+    }
+    case 4: {
+        console.log("Thursday");
+    }
+    case 5: {
+        console.log("friday");
+    }
+    default:
+        console.log("weekend");
+        break;
+}
